fa0512cf31f606e660c3d2520ae938cfb0dc2eb0
Memitaru <amandatwah@gmail.com>
2019-06-18 23:03:32 -0700

    Completed lecture

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,6 +2,11 @@ module.exports = {
   add,
 };
 
-function add() {
-  return null;
+function add(x) {
+
+  const args = Array.isArray(x) ? x : Array.from(arguments)
+
+  return args.reduce((acc, val) => {
+    return acc + val;
+  }, 0)
 }
--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,35 @@
 const { add } = require('./calculator.js');
 
 // test away!
+
+
+describe('the add function', () => {
+    
+    it('should add two values', () => {
+        const result = add(2,2)
+
+        // assertion
+        expect(result).toBe(4);
+        expect(add(-1,4)).toBe(3);
+        expect(add(0,6)).toBe(6);
+    });
+
+    it('should return 0 when no arguements are provided', () => {
+        expect(add()).toBe(0);
+    })
+
+    it('should return the passed in value when only one arguement is provided', () => {
+        expect(add(5)).toBe(5);
+        expect(add(1)).toBe(1);
+    })
+
+    it('should add any number of arguements', () => {
+        expect(add(1, 2, 3)).toBe(6);
+        expect(add(1, 1, 10, 2, 4, 1, 0)).toBe(19);
+    })
+
+    it('should acceept an array as an arguement', () => {
+        expect(add([1,2,3,4])).toBe(10);
+    })
+    
+});
